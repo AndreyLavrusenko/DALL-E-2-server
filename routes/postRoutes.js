d4068bd3faa41a04f3f43d59d800315f644d5ff9
andreylavrusenko <m>
2023-02-07 15:57:48 +0300

    finish

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,7 +7,7 @@ const Post = require('../mongodb/models/post')
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.OPENAI_API_KEY,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
