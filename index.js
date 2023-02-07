1ae17000bfa1d18a0611b87a8ea76837f9f8f17a
andreylavrusenko <m>
2023-02-07 17:04:15 +0300

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 const app = express();
 
-app.use(cors({credentials: true, origin: [process.env.CLIENT_URI]}))
+app.use(cors({credentials: true, origin: process.env.CLIENT_URI}))
 app.use(express.json({limit: '50mb'}))
 
 app.use('/api/v1/post', postRoutes)
